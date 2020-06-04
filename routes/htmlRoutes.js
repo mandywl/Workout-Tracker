f11be877d206fcf97dd61b1388a98f41e8d3c9a7
mandywl <Mengsi.Wells-Lakeland@vu.edu.au>
2020-06-04 22:08:40 +1000

    Modified htmlRoute

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,18 @@
 var path = require("path");
 
 module.exports = function (app) {
-/*   app.get("*", function (req, res) {
-    debugger;
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  }); */
   app.get("/", function (req, res) {
     debugger;
-    console.log("path is ", path.join(__dirname, "../public/exercise.html"));
-    //res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    res.render("hello")
-
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-    app.get("/stats", function(req, res) {
-    //res.sendFile(path.join(__dirname, "../public/stats.html"));
-    res.render("hello")
+
+  app.get("/exercise", function (req, res) {
+    debugger;
+    console.log("path is ", path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 };
